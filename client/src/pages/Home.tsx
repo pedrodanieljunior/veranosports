777ecc8e411fd823354f24c044a4be92ab2ecf0e
pedrodanieljuni <46058950-pedrodanieljuni@users.noreply.replit.com>
2026-03-13 14:49:00 +0000

    Task #15: Ajustar espaçador do banner desktop para 9vh
    Changed the desktop spacer height from "20vh" to "9vh" to position
    the banner closer to the search/buttons area above it.
    
    File changed: client/src/pages/Home.tsx (line 322)
    No mobile changes.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -316,7 +316,7 @@ export default function Home() {
             </div>
 
             {/* Spacer to push content down to align with sidebar leagues */}
-            <div className="flex-shrink-0" style={{ height: "20vh" }} />
+            <div className="flex-shrink-0" style={{ height: "9vh" }} />
 
             {/* Promo banners */}
             <div className="pb-4 flex-shrink-0" style={{ paddingRight: "8vw" }}>
