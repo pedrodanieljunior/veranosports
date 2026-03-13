9a336d569483ec2ef17f35e6bc2471830025a41e
pedrodanieljuni <46058950-pedrodanieljuni@users.noreply.replit.com>
2026-03-13 14:20:22 +0000

    Task #9: Baixar levemente o banner no desktop
    Increased the spacer height before the desktop banner carousel
    from "24vh" to "30vh" so the banner sits lower on the page.
    
    File changed: client/src/pages/Home.tsx (line 322)
    No mobile changes. No other layout changes.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -316,7 +316,7 @@ export default function Home() {
             </div>
 
             {/* Spacer to push content down to align with sidebar leagues */}
-            <div className="flex-shrink-0" style={{ height: "24vh" }} />
+            <div className="flex-shrink-0" style={{ height: "30vh" }} />
 
             {/* Promo banners */}
             <div className="pb-4 flex-shrink-0" style={{ paddingLeft: "3vw", paddingRight: "1vw" }}>
